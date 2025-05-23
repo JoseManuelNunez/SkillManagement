97f88096261480d1951e6511cdfe4c3ee8625e9a
sebastiancorcino <sebastiancorcino@gmail.com>
2025-05-22 23:38:22 -0400

    add: morgan logger

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import morgan from 'morgan';
 import cors from 'cors';
 import { Config } from './config';
 import setupRoutes from './routes';
@@ -7,6 +8,7 @@ import { SeederService } from './services/seeder';
 import { SkillService } from './services/skill';
 import { ProjectService } from './services/project';
 import database from './data-source';
+
 
 
 const app = express();
@@ -23,14 +25,16 @@ export async function getServer(): Promise<Express> {
     ProjectService.init(dataSource);
     SeederService.init(dataSource);
 
-    // TODO: Morgan config
 
+    // Trigger seeder
     const seederService = new SeederService();
     await seederService.fill();
 
 
+    // API Config
     app.use(cors());
     app.use(express.json())
+    app.use(morgan('dev'));
     setupRoutes(app);
 
     return app;
